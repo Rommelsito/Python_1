867869711fce69306f2f74a4f6796c2a5817e135
Rommel <momerg2016@gmail.com>
2024-11-04 18:49:32 -0400

    4to commit

--- python/prueba1.py
+++ python/prueba1.py
@@ -2,57 +2,60 @@
 #MENU DE OPCIONES
 #BUCLE PARA REPETIR LA TAREA OTRA VEZ
 
-continuar = "si"
+continuar = True
 
-while continuar.lower() == "si":
+while continuar:
 
-    print(" \nOPCIONES.")
+    print(" \n OPCIONES.")
     print("<1>.Sumar")
     print("<2>.Restar")
     print("<3>.Multiplicar")
-    print("<4>.Dividir \n")
+    print("<4>.Dividir")
+    print("<5>.Salir \n")
     
     opcion = int(input("INGRESA TU OPCION: "))
     
     if opcion == 1:
-        print("VAMOS A SUMAR: ")
+        print("VAMOS A SUMAR \n")
     
         num1 = int(input("Introduce un numero: "))
         num2 = int(input("Introduce otro numero: "))
     
         sum = num1 + num2
-        print(F"El resultado es {sum} \n")
+        print(F"EL RESULTADO ES: {sum} \n")
     
     elif opcion == 2:
-        print("VAMOS A RESTAR:")
+        print("VAMOS A RESTAR \n")
         
         num1 = int(input("Introduce un numero: "))
         num2 = int(input("Introduce otro numero: "))
     
         rest = num1 - num2
-        print(F"El resultado es {rest} \n")
+        print(F"EL RESULTADO ES: {rest} \n")
     
     elif opcion == 3:
-        print("VAMOS A MULTIPLICAR")
+        print("VAMOS A MULTIPLICAR \n")
     
         num1 = int(input("Introduce un numero: "))
         num2 = int(input("Introduce otro numero: "))
     
         mult = num1 * num2
-        print(F"El resultado de la multiplicacion es {mult} \n")
+        print(F"EL RESULTADO ES: {mult} \n")
     
     elif opcion == 4:
-        print("VAMOS A DIVIDIR")
+        print("VAMOS A DIVIDIR \n")
     
         num1 = int(input("Introduce un numero: "))
         num2 = int(input("Introduce otro numero: "))
     
         div = num1 // num2
-        print(F"El resultado de la division es {div} \n")
+        print(f"EL RESULTADO ES: {div}")
+
+    elif opcion == 5:
+        continuar = False
+        print("Saliendo... \n")
     
     else:
-        print("OPCION INVALIDA \n")
-    
-    continuar = input("QUIERES CONTINUAR(SI/NO)?:  ")
+        print("OPCION INVALIDA")
 
 print("FIN DEL PROGRAMA")
